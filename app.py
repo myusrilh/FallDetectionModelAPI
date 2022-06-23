84146ec133c696c696e584b9c73e98077bdc8d78
myusrilh <yusrilhasriansyah@gmail.com>
2022-06-23 08:01:34 +0700

    update model API

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
-# import pickle
 import joblib
 import pandas as pd
-import time,datetime
-import json
+import time
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,30 +17,44 @@ parser.add_argument('payload')
 class FallsClassifier(Resource):
     def post(self):
         args = parser.parse_args()
-        # model = joblib.load('ml_model_decision_tree_fall_system_Entropy_sklearn.pkl')
-        # X = np.array(json.loads(args['data']))
         payload = eval(args['payload'])
-        # dataset_id = args['dataset_ID']
-        # print(data, dataset_id)
+        
         X = pd.DataFrame().from_dict(payload)
         
         dt_before_pred = time.strftime('%A, %d %B %Y %H:%M:%S')
-        # dt_before_pred = datetime.datetime.now().strftime("%A, %d %B %Y, %H:%M:%S.%f")
         prediction = model.predict(X)[0]
         dt_after_pred = time.strftime('%A, %d %B %Y %H:%M:%S')
-        # dt_after_pred = datetime.datetime.now().strftime("%A, %d %B %Y, %H:%M:%S.%f")
         
-        if prediction == 1:
-            prediction = "fall"
-        elif prediction == 0:
-            prediction = "not fall"
+        # 0 = standing
+        # 1 = walking
+        # 2 = free fall
+        # 3 = lying down
+        # 4 = wake up
+        # 5 = slowly goes down
+        # 6 = jogging
+        # 7 = wake to sit
+        # 8 = sit
+        if prediction == 0:
+            prediction = "Berdiri"
+        elif prediction == 1:
+            prediction = "Berjalan"
+        elif prediction == 2:
+            prediction = "Terjatuh!"
+        elif prediction == 3:
+            prediction = "Posisi di lantai"
+        elif prediction == 4:
+            prediction = "Bangkit"
+        elif prediction == 5:
+            prediction = "Turun perlahan"
+        elif prediction == 6:
+            prediction = "Berlari pelan"
+        elif prediction == 7:
+            prediction = "Bangkit ke posisi duduk"
+        elif prediction == 8:
+            prediction = "Duduk"
         
-        # return jsonify(list(json.dumps(prediction)))
-        # response = {"response": "OK","prediction": prediction,"seconds":args['time'],"datetime":time.strftime('%A %B, %d %Y %H:%M:%S')}
         response = {"prediction": prediction,"dt_before_pred":dt_before_pred,"dt_after_pred":dt_after_pred}
         
-        
-        # response = {"response": "OK", "value": json.dumps(payload)}
         return jsonify(response)
 
 api.add_resource(FallsClassifier, '/falls')
@@ -50,9 +62,7 @@ api.add_resource(FallsClassifier, '/falls')
 if __name__ == '__main__':
     # Load model
     
-    # model_name = 'decision_tree_fall_system_kalman_filter_30mei2022_entropy_sklearn.sav'
-    # model_name = 'decision_tree_fall_system_kalman_filter_31mei2022_entropy_sklearn.h5'
-    model_name = 'decision_tree_fall_system_complementary_filter_2juni2022_entropy_sklearn.h5'
+    model_name = 'complementary_filter_21juni2022_8labelclass.h5'
     
     with open('model/'+model_name, 'rb') as f:
         model = joblib.load(f)
