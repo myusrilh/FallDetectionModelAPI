bd8c9fbfd1a8a4f086beedc0ed9ec6ada41b96c0
myusrilh <yusrilhasriansyah@gmail.com>
2022-07-09 15:05:05 +0700

    update heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:fall
+web: gunicorn app:fall
--- app.py
+++ app.py
@@ -5,41 +5,41 @@ import pandas
 from datetime import datetime
 
 fall = Flask(__name__)
-# api = Api(fall)
+api = Api(fall)
 
-# parser = reqparse.RequestParser()
-# parser.add_argument('dataset_ID')
-# parser.add_argument('time')
-# parser.add_argument('payload')
+parser = reqparse.RequestParser()
+parser.add_argument('dataset_ID')
+parser.add_argument('time')
+parser.add_argument('payload')
 
-# class FallsClassifier(Resource):
-#     def post(self):
+class FallsClassifier(Resource):
+    def post(self):
         
-#         model_name = 'kfall_complementary_filter_8juli2022_3label.h5'
+        model_name = 'kfall_complementary_filter_8juli2022_3label.h5'
     
-#         with open('model/'+model_name, 'rb') as f:
-#             model = joblib.load(f)
+        with open('model/'+model_name, 'rb') as f:
+            model = joblib.load(f)
         
-#         args = parser.parse_args()
-#         payload = eval(args['payload'])
+        args = parser.parse_args()
+        payload = eval(args['payload'])
         
-#         X = pandas.DataFrame().from_dict(payload)
+        X = pandas.DataFrame().from_dict(payload)
         
-#         dt_before_pred = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
-#         prediction = model.predict(X)[0]
-#         dt_after_pred = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
+        dt_before_pred = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
+        prediction = model.predict(X)[0]
+        dt_after_pred = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
         
         
-#         response = {"prediction":str(prediction),"dt_before_pred":dt_before_pred,"dt_after_pred":dt_after_pred}
+        response = {"prediction":str(prediction),"dt_before_pred":dt_before_pred,"dt_after_pred":dt_after_pred}
         
-#         return jsonify(response)
+        return jsonify(response)
 
-# api.add_resource(FallsClassifier, '/falls')
+api.add_resource(FallsClassifier, '/falls')
 
-@fall.route('/')
-def index():
-    # A welcome message to test our server
-    return render_template("index.html")
+# @fall.route('/')
+# def index():
+#     # A welcome message to test our server
+#     return render_template("index.html")
 
 if __name__ == '__main__':
     # model_name = 'kfall_complementary_filter_7juli2022_3label.h5'
